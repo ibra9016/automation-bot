dc02e16a43c7eab0938b269e3a4871935a2cac1b
ibrahim <ibrahim.ahmad1597@gmail.com>
2025-08-02 22:57:54 +0300

    final touches

--- bot.js
+++ bot.js
@@ -29,7 +29,13 @@ async function runScript(formData) {
   ]);
 
   for (let key in formData) {
-      if(key === 'state' || key === 'productUrl' || key === 'shoeSize' || key === 'email') continue;
+      if(key === 'productUrl' || key === 'shoeSize') continue;
+
+      if(key === 'state'){
+        await page.waitForSelector('[name="zone"]')
+        await page.select('[name="zone"]', formData[key]);
+        continue;
+      }
       if(key === 'number' ){
         const cardNumberFrameHandle = await page.waitForSelector('iframe.card-fields-iframe[title="Field container for: Card number"]');
         const cardNumberFrame = await cardNumberFrameHandle.contentFrame();
@@ -40,51 +46,36 @@ async function runScript(formData) {
       if(key === 'verification_value' ){
         const cardNumberFrameHandle = await page.waitForSelector('iframe.card-fields-iframe[title="Field container for: Security code"]');
         const cardNumberFrame = await cardNumberFrameHandle.contentFrame();
-        await cardNumberFrame.waitForSelector('input')
-        await cardNumberFrame.type('input',formData[key]);
-        continue;
+
+        // Wait for the input with id or name "expiry"
+              const verificationInput = await cardNumberFrame.waitForSelector('input#verification_value, input[name="verification_value"]', { visible: true });    
+              
+              await verificationInput.focus();
+
+              await verificationInput.type(formData[key], { delay: 100 });
+
+              continue;
       }
       if(key === 'expiry' ){
         const cardNumberFrameHandle = await page.waitForSelector('iframe.card-fields-iframe[title="Field container for: Expiration date (MM / YY)"]');
-        const cardNumberFrame = await cardNumberFrameHandle.contentFrame();
-        await cardNumberFrame.waitForSelector('input')
-        await cardNumberFrame.type('input',formData[key]);
-        continue;
+        const expiryFrame = await cardNumberFrameHandle.contentFrame();
+
+        // Wait for the input with id or name "expiry"
+              const expiryInput = await expiryFrame.waitForSelector('input#expiry, input[name="expiry"]', { visible: true });    
+              
+              await expiryInput.focus();
+
+              await expiryInput.type(formData[key], { delay: 100 });
+
+              continue;
       }
-      await page.waitForSelector('[name="'+key+'"]')
+     await page.waitForSelector('[name="'+key+'"]')
      await page.click('[name="'+key+'"]')
      await page.type('[name="'+key+'"]',formData[key])
 }
  
-    // await page.waitForSelector('[name="firstName"]')
-    // await page.click('[name="firstName"]')
-    // await page.type('[name="firstName"]',formData.firstName);
-
-    // await page.waitForSelector('[name="lastName"]')
-    // await page.click('[name="lastName"]')
-    // await page.type('[name="lastName"]',formData.lastName);
-
-    // await page.waitForSelector('[name="address1"]')
-    // await page.click('[name="address1"]')
-    // await page.type('[name="address1"]',formData.address);
-    
-    // if(formData.optionalAddress != "");{
-    //   await page.waitForSelector('[name="address2"]')
-    //   await page.click('[name="address2"]')
-    //   await page.type('[name="address2"]',formData.optionalAddress);
-    // }
-
-    // await page.waitForSelector('[name="city"]')
-    // await page.click('[name="city"]')
-    // await page.type('[name="city"]',formData.city);
-
-    // await page.waitForSelector('[name="city"]')
-    // await page.click('[name="city"]')
-    // await page.type('[name="city"]',formData.city);
-
-
-    
-
+    await page.click('[id^="DeprecatedCheckbox"]');
+    await page.click('#checkout-pay-button');
 
 
 }
